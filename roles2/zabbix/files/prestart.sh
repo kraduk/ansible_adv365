606b3a6637dfa1e7c79c6bbb1a0897c972b2d2bf
kraduk <kraduk@gmail.com>
2016-08-17 09:27:28 +0100

    rewobble loads of stuff

--- roles2/zabbix/files/prestart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Hostname="`hostname| tr 'A-Z' 'a-z'` > /etc/zabbix/agentd.conf.d/host.conf
-/sbin/ip addr show mgmt | awk '/ inet / { gsub(/\/[0-9]+$/,"",$2); print "ListenIP="$2}' > /etc/zabbix/agentd.conf.d/listen.conf
-/usr/sbin/zabbix_agentd -c /etc/zabbix/agentd.conf
-
-
